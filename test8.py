45b6e6f4e376fa4ecf6c5513be0bd25bd6c15881
Vladimir22222222 <vovka2355@gmail.com>
2024-07-27 15:35:54 +0300

    a

--- test8.py
+++ test8.py
@@ -1,7 +1,7 @@
 n = int(input("Введите количество чисел "))
 zero = 0
-for i in range(0, n):
-	num = int(input("введите целое число "))
-if num==0:
-  zero +=1
+for i in range(1, n+1):
+	s = int(input("введите целое число "))
+	if s == 0:
+				zero += 1
 print(zero)
